8ae74aaaec2407eae064f7a64f7cafd4032bca51
Monica Barboza <145809292+MonBoza@users.noreply.github.com>
2023-10-20 15:56:39 -0500

    change form back to radio update JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,25 @@
 function hideResults(){
-    document.getElementById("question1").setAttribute("class", "hidden");
-    document.getElementById("question2").setAttribute("class", "hidden");
-    document.getElementById("question3").setAttribute("class", "hidden");
-    document.getElementById("question4").setAttribute("class", "hidden");
-    document.getElementById("question5").setAttribute("class", "hidden");
+    document.getElementById("swift").setAttribute("class", "hidden");
+    document.getElementById("ruby").setAttribute("class", "hidden");
+    document.getElementById("sharp").setAttribute("class", "hidden");
 }
 
-function selectLanguage(event){
+function handleRadio(event){
+    hideResults();
     event.preventDefault();
-    
-    
-}
+    const foodSelect = document.querySelector("input[name='food']checked".value);
+    const seasonSelect = document.querySelector("input[name='season']checked".value);
+    const jobSelect = document.querySelector("input[name='jobs']checked".value);
+    const bookSelect = document.querySelector("input[name='books']checked".value);
+    const picSelect = document.querySelector("input[name='picture']checked".value);
+    console.log();
+    if ((foodSelect==="pizza")&&(seasonSelect==="autumn")&&(jobSelect==="Dev")&&(bookSelect==="fiction")&&(picSelect==="quokka")){ 
+        document.getElementById("swift").removeAttribute("class"); 
+    } else if((foodSelect==="fries")&&(seasonSelect==="summer")&&(jobSelect==="web")&&(bookSelect==="horror")&&(picSelect==="raccoon")){ 
+        document.getElementById("sharp").removeAttribute("class");
+    } else if (())
+  }
+
+window.addEventListener("load", function(){
+    document.getElementById("radio-form").addEventListener("submit", handleRadio);
+});
