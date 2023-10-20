1329ede865f74088a8c0ed7ba3d8838556ee2bca
Monica Barboza <145809292+MonBoza@users.noreply.github.com>
2023-10-20 14:57:16 -0500

    change from radio form in html

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+function hideResults(){
+    document.getElementById("question1").setAttribute("class", "hidden");
+    document.getElementById("question2").setAttribute("class", "hidden");
+    document.getElementById("question3").setAttribute("class", "hidden");
+    document.getElementById("question4").setAttribute("class", "hidden");
+    document.getElementById("question5").setAttribute("class", "hidden");
+}
+
+function selectLanguage(event){
+    event.preventDefault();
+    
+    
+}
