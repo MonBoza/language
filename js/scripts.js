2fc72febc208e05a5fddc4dd274443b683cfa0d2
Monica Barboza <145809292+MonBoza@users.noreply.github.com>
2023-10-25 14:42:24 -0500

    indentation, whitespace and README errors

--- js/scripts.js
+++ js/scripts.js
@@ -15,16 +15,16 @@ function langSelect(event){
     const foodInput = document.getElementById("question3").value;
     const bookInput = document.getElementById("question4").value;
     const vacationInput = document.getElementById("question5").value;
-    if (careerInput === "developer"&& foodInput==="pizza" && bookInput==="fiction" && seasonInput==="summer" &&vacationInput==="hawaii"){
+    if (careerInput ==="developer"&&foodInput==="pizza"&&bookInput==="fiction"&&seasonInput==="summer"&&vacationInput==="hawaii"){
         document.getElementById("sharp").removeAttribute("class");
-    } else if (careerInput=== "dev ops" && foodInput==="sushi" && bookInput==="horror" && seasonInput==="winter" &&vacationInput==="spain") {
-    document.getElementById("swift").removeAttribute("class");
-    } else if (careerInput === "backend developer" && foodInput ==="pizza"&& bookInput==="mystery"&&seasonInput==="winter"&&vacationInput==="italy"){
-    document.getElementById("swift").removeAttribute("class");
-    } else if (careerInput === "cyber security"&&foodInput==="tacos"&&bookInput==="fiction"&& seasonInput==="spring"&&vacationInput==="mexico")
+    } else if (careerInput==="dev ops"&&foodInput==="sushi"&&bookInput==="horror"&&seasonInput==="winter"&&vacationInput==="spain") {
+        document.getElementById("swift").removeAttribute("class");
+    } else if (careerInput === "backend developer"&&foodInput ==="pizza"&& bookInput==="mystery"&&seasonInput==="winter"&&vacationInput==="italy"){
+        document.getElementById("swift").removeAttribute("class");
+    } else if (careerInput=== "cyber security"&&foodInput==="tacos"&&bookInput==="fiction"&&seasonInput==="spring"&&vacationInput==="mexico"){
         document.getElementById("sharp").removeAttribute("class");
-    else document.getElementById("ruby").removeAttribute("class");
-    clearForm();
+    } else document.getElementById("ruby").removeAttribute("class");
+     clearForm();
 }
 
 window.addEventListener("load",function() {
